2e2d71fe6e6f1ab86fae30f196a85945e13d10dd
Lead Developer (Dynasty Bot) <bradlashard@gmail.com>
2026-03-24 19:21:03 -0500

    fix(db): implement prisma client singleton

--- apps/frontend/src/lib/prisma.ts
+++ apps/frontend/src/lib/prisma.ts
@@ -1,13 +1,15 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
 }
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient()
+  global.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma
-}
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
